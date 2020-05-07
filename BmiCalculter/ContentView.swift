5f35bcb913d0375da63d39655213e9699027026b
Saad <a771367@gmail.com>
2020-05-07 22:37:04 +0300

    Initial Commit

---
+++ BmiCalculter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BmiCalculter
+//
+//  Created by saad on 5/7/20.
+//  Copyright © 2020 Saad Almutairi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
