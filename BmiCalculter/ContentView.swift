3013627767819c8d577a20c1044f6aee76ef40a9
Saad <a771367@gmail.com>
2020-05-09 21:33:17 +0300

    s

--- BmiCalculter/ContentView.swift
+++ BmiCalculter/ContentView.swift
@@ -9,9 +9,95 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State var height : String = ""
+    @State var width : String = ""
+    
+    
+    
+    @State var sum : Int = 0
+    
+    
+    
+     @State var bmiC = "0.0"
+    @State var BmiLabel = "-"
+    
+    
+    
+    func BmiCalc(width : Double,height:Double) -> Int {
+            self.sum = Int(width / (height * height))
+            return sum
+        }
+    func StringRu(bmi : Int) -> String {
+        self.bmiC = String(bmi)
+        
+    if bmi < 20 {
+        self.BmiLabel = "🦎"
+        }
+        else if bmi >= 20 && bmi < 25 {
+             self.BmiLabel = "🤩"
+        }
+        else if bmi > 25 {
+         self.BmiLabel = "🥺"
+        }
+        
+        return bmiC
     }
+    
+    var body: some View {
+        
+    
+        
+        HStack {
+            Spacer()
+        VStack {
+             Spacer()
+            Text("Welcome to My BMI")
+                   .font(.system(size: 40))
+                          .bold()
+            TextField("Hight ", text: $height)
+                .font(.largeTitle)
+            TextField("Width ", text: $width)
+                .font(.largeTitle)
+             
+            Button(action: {
+            
+                self.sum = self.BmiCalc(width: Double(self.width)!, height: Double(self.height)!)
+                self.StringRu(bmi: self.sum)
+                
+            }) {
+               
+                Text("calc")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                .bold()
+                .frame(width: 200, height: 100)
+                    .background(Color.blue)
+                .clipShape(Capsule())
+                
+                
+              
+                
+            }
+            
+            Text("BMI")
+                        .font(.system(size: 40))
+                .bold()
+            Text("\(bmiC)")
+                                   .font(.system(size: 40))
+             .bold()
+                       
+            Text("Statues")
+                        .font(.system(size: 40))
+             .bold()
+            Text("\(BmiLabel)")
+                .font(.system(size: 40))
+             .bold()
+                
+             Spacer()
+        }
+            
+             Spacer()
+        }  }
 }
 
 struct ContentView_Previews: PreviewProvider {
